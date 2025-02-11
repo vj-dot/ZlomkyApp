3cd333b7eb47d2da6ff09c8836db208c7b207e92
Vojtěch Jirásek <78324879+vj-dot@users.noreply.github.com>
2025-02-11 16:42:42 +0100

    asdfghjkl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello and welcome!");
         System.out.println("pokus");
         System.out.println("gdashgdhsa");
+        System.out.println("ahojahojahoj");
 
     }
 }
