7084399d45975e24a6ab1d28e9b0b9ba72ad9ad8
Vojtěch Jirásek <78324879+vj-dot@users.noreply.github.com>
2025-02-17 11:26:24 +0100

    added aritmetic operations on Zlomek

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,19 @@ public class Main {
         System.out.println("Zlomkoaplikace");
 
         //vytvoreni instance(=objekt) tridy zlomek
-        Zlomek a = new Zlomek(1, 2);
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(2, 5);
 
         System.out.printf("Zlomek a =  %s\n", a.toString());
+        System.out.printf("Zlomek b =  %s\n", b.toString());
+
+        Zlomek krat = a.krat(b).zkratit();
+        System.out.printf("Zlomek a * b = %s\n", krat.toString());
+        Zlomek deleno = a.deleno(b).zkratit();
+        System.out.printf("Zlomek a / b = %s\n", deleno.toString());
+        Zlomek plus = a.plus(b).zkratit();
+        System.out.printf("Zlomek a + b = %s\n", plus.toString());
+        Zlomek minus = a.minus(b).zkratit();
+        System.out.printf("Zlomek a - b = %s\n", minus.toString());
     }
 }
--- src/main/java/cz/uhk/zlomky/model/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/model/Zlomek.java
@@ -1,8 +1,8 @@
 package cz.uhk.zlomky.model;
 
 public class Zlomek {
-    private int citatel;
-    private int jmenovatel;
+    private final int citatel;
+    private final int jmenovatel;
 
 
     public int getCotatel() {
@@ -15,6 +15,45 @@ public class Zlomek {
     public Zlomek(int citatel, int jmenovatel) {
         this.citatel = citatel;
         this.jmenovatel = jmenovatel;
+    }
+
+    // operace se zlomky
+    public Zlomek krat(Zlomek b) {
+        Zlomek c = new Zlomek(citatel * b.citatel, jmenovatel * b.jmenovatel);
+        return c;
+    }
+
+    public Zlomek deleno(Zlomek b) {
+        Zlomek c = new Zlomek(citatel * b.jmenovatel, jmenovatel * b.citatel);
+        return c;
+    }
+
+    public Zlomek plus(Zlomek b) {
+        Zlomek c = new Zlomek(citatel * b.jmenovatel + b.citatel * jmenovatel, jmenovatel * b.jmenovatel);
+        return c;
+    }
+
+    public Zlomek minus(Zlomek b) {
+        Zlomek c = new Zlomek(citatel * b.jmenovatel - b.citatel * jmenovatel, jmenovatel * b.jmenovatel);
+        return c;
+    }
+
+    public Zlomek zkratit() {
+        int a = Math.abs(citatel);
+        int b = Math.abs(jmenovatel);
+        if (a < b) {
+            int pom = a;
+            a = b;
+            b = pom;
+        }
+
+        int zb;
+        do {
+            zb = a % b;
+            a = b;
+            b = zb;
+        } while (zb != 0);
+        return new Zlomek(citatel / a, jmenovatel / a);
     }
 
     @Override
