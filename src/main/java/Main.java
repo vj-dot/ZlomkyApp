d9ce04e59650f3649a1d9a2943b2cd7c76305e0b
Vojtěch Jirásek <78324879+vj-dot@users.noreply.github.com>
2025-02-24 10:32:29 +0100

    added testing of Zlomek

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,36 @@ public class Main {
         //vytvoreni instance(=objekt) tridy zlomek
         Zlomek a = new Zlomek(3, 4);
         Zlomek b = new Zlomek(2, 5);
+        Zlomek c = new Zlomek(9, 10);
+        Zlomek d = new Zlomek(5, 6);
 
         System.out.printf("Zlomek a =  %s\n", a.toString());
         System.out.printf("Zlomek b =  %s\n", b.toString());
+        System.out.printf("Zlomek c =  %s\n", c.toString());
+        System.out.printf("Zlomek d =  %s\n", d.toString());
 
-        Zlomek krat = a.krat(b).zkratit();
-        System.out.printf("Zlomek a * b = %s\n", krat.toString());
-        Zlomek deleno = a.deleno(b).zkratit();
-        System.out.printf("Zlomek a / b = %s\n", deleno.toString());
-        Zlomek plus = a.plus(b).zkratit();
-        System.out.printf("Zlomek a + b = %s\n", plus.toString());
-        Zlomek minus = a.minus(b).zkratit();
-        System.out.printf("Zlomek a - b = %s\n", minus.toString());
+        System.out.printf("Zlomek a * b = %s\n", a.krat(b).zkratit());
+        System.out.printf("Zlomek a / b = %s\n", a.deleno(b).zkratit());
+        System.out.printf("Zlomek a + b = %s\n", a.plus(b).zkratit());
+        System.out.printf("Zlomek a - b = %s\n", a.plus(b).zkratit());
+
+        Zlomek[] poleZlomku = new Zlomek[4];
+        poleZlomku[0] = a;
+        poleZlomku[1] = b;
+        poleZlomku[2] = c;
+        poleZlomku[3] = d;
+
+        Zlomek all = sumaZlomku(poleZlomku);
+        System.out.printf("Soucet zlomku a, b, c, d je %s\n", all);
+    }
+
+    public static Zlomek sumaZlomku(Zlomek[] pole) {
+        Zlomek soucet = new Zlomek(0, 1);
+
+        for (Zlomek z : pole) {
+            soucet = soucet.plus(z); // soucet += z
+        }
+
+        return soucet.zkratit();
     }
 }
---
+++ src/test/java/cz/uhk/zlomky/model/ZlomekTest.java
@@ -0,0 +1,26 @@
+package cz.uhk.zlomky.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZlomekTest {
+
+    @org.junit.jupiter.api.Test
+    void krat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleno() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void zkratit() {
+    }
+}
